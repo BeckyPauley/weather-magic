e72dc185f6b8a17845214b98befdb955ed4893d1
becky <64131207+BeckyPauley@users.noreply.github.com>
2021-11-13 18:48:39 +0000

    add: environment variable functionality

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -15,11 +19,20 @@ func main() {
 
 	fmt.Printf("Getting the weather for: %s\n", cityVar)
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	weatherKey := os.Getenv("WEATHER_KEY")
+
+	weatherApi := "api.openweathermap.org/data/2.5/weather?q=" + cityVar + "&appid=" + weatherKey
+
 	// welcome message - done
 	// prompt for input (city name) - done
 	// check- print input (city name) - done
 	// parse input - done
-	// set url for open weather api as a variable
+	// set url for open weather api as a variable - done
 	// request to api
 	// parse api response
 	// return result
