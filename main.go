5ac88039ddd5daf1b52002c94c6390d86f680846
becky <64131207+BeckyPauley@users.noreply.github.com>
2021-11-13 19:32:43 +0000

    add: extract city name from json response

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -10,6 +12,10 @@ import (
 
 	"github.com/joho/godotenv"
 )
+
+type name struct {
+	Name string `json:"name"`
+}
 
 func main() {
 	fmt.Println("Welcome to Weather Magic!\n")
@@ -44,12 +50,32 @@ func main() {
 
 	fmt.Printf("HTTP status: %s\n", res.Status)
 
+	w := name{}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(body, &w)
+	if err != nil {
+		log.Fatal(err)
+		log.Fatalf("unable to parse value: %q, error: %s",
+			string(body), err.Error())
+	}
+
+	fmt.Println(w)
+
 	// welcome message - done
 	// prompt for input (city name) - done
 	// check- print input (city name) - done
 	// parse input - done
 	// set url for open weather api as a variable - done
 	// request to api - done (200)
-	// parse api response
+	// parse api response - basic done
 	// return result
 }
